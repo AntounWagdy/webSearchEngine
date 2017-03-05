b67d8c1dbccaad7b4c565e7ace9c434746da8853
AntounWagdy <AntounWagdy@gmail.com>
2017-03-05 16:48:47 +0200

    httpRequestHandler

--- src/java/webSearchEngine/httpRequestHandler.java
+++ src/java/webSearchEngine/httpRequestHandler.java
@@ -1,24 +1,63 @@
-
 package webSearchEngine;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import java.net.URL;
-import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author Antoun
- */
 public class httpRequestHandler {
-	boolean request()
-	{
-		return false;
-	}
-	List<URL> getUrls()
-	{
-		return null;	
-	}
-	String getBody()
-	{
-		return null;
-	}    
+
+    private Document Doc;
+
+    boolean check_type_html(URL url) {
+        try {
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("HEAD");
+            String type = con.getContentType();
+            con.disconnect();
+            if (type.startsWith("text/html"))
+            {
+                return true;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(httpRequestHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return false;
+    }
+
+    void downloadPage(URL url) {
+        try {
+            Doc = Jsoup.connect(url.toString()).get();
+        } catch (IOException ex) {
+            Logger.getLogger(httpRequestHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    ArrayList<URL> getUrls() {
+        ArrayList<URL> myList = new ArrayList<>();
+        try {
+            Elements links = Doc.select("a[href]");
+            String urll;
+            for (Element link : links) {
+                urll = link.absUrl("href");
+                myList.add(new URL(urll));
+            }
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(httpRequestHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return myList;
+    }
+
+    String getBody() {
+        return Doc.body().text();
+    }
+
 }
