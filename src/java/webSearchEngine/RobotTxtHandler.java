cfed6772614d9203e7b439867af2b62423d6e277
AntounWagdy <AntounWagdy@gmail.com>
2017-03-05 22:52:52 +0200

    Exception Handling

--- src/java/webSearchEngine/RobotTxtHandler.java
+++ src/java/webSearchEngine/RobotTxtHandler.java
@@ -12,80 +12,83 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class RobotTxtHandler {
-	private String userAgent = "Googlebot";
-	private URL base;
-	private BufferedReader robotData;
-	private ArrayList<URL> Disallow;
-	private ArrayList<URL> Allow;
-	private Integer crawlDelay = 0;
 
-	/* Preparing the URL to Connect to */
-	public RobotTxtHandler(URL url) {
-		Disallow = new ArrayList<URL>();
-		
-		try {
-			base = new URL(url.getProtocol() + "://" + url.getHost() + (url.getPort() > -1 ? ":" + url.getPort() : ""));
-			String hostId = base + "/robots.txt";
-			
-			URL hostURL = new URL(hostId);
-			URLConnection connection = hostURL.openConnection();
+    private String userAgent = "Googlebot";
+    private URL base;
+    private BufferedReader robotData;
+    private ArrayList<URL> Disallow;
+    private ArrayList<URL> Allow;
+    private Integer crawlDelay = 0;
 
-			robotData = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    /* Preparing the URL to Connect to */
+    public RobotTxtHandler(URL url) {
+        Disallow = new ArrayList<URL>();
 
-			extractData();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        try {
+            base = new URL(url.getProtocol() + "://" + url.getHost() + (url.getPort() > -1 ? ":" + url.getPort() : ""));
+            String hostId = base + "/robots.txt";
+            URL hostURL = new URL(hostId);
+            URLConnection connection = hostURL.openConnection();
+            robotData = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            extractData();
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(RobotTxtHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(RobotTxtHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-	private void extractData() {
-		String inputLine;
-		String[] inputLineArr = { null, null };
-		try {
-			while ((inputLine = robotData.readLine()) != null) {
-				if (inputLine.equals("User-agent: *") || inputLine.equals("User-agent: " + userAgent)) {
-					while ((inputLine = robotData.readLine()) != null && !inputLine.matches("User-agent:(.*)")) {
-						inputLineArr = inputLine.split(" ");
-						if (inputLineArr[0].equals("Disallow:")) {
-							if (inputLineArr[1] == null) {
-								// accept all (do nothing)
-							} else if (inputLineArr[1].equals("/")) {
-								// Refuse all
-								//TODO:what to do
-							} else {
-								Disallow.add(new URL(base+inputLineArr[1].trim()));
-							}
-						} else if (inputLineArr[0].equals("Crawl-delay:")) {
-								crawlDelay = Integer.parseInt(inputLineArr[1]);
-						} else if(inputLineArr[0].equals("Allow:")) {
-								Allow.add(new URL(base+inputLineArr[1].trim()));
-						} 
-					}
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    private void extractData() {
+        String inputLine;
+        String[] inputLineArr = {null, null};
+        try {
+            while ((inputLine = robotData.readLine()) != null) {
+                if (inputLine.equals("User-agent: *") || inputLine.equals("User-agent: " + userAgent)) {
+                    while ((inputLine = robotData.readLine()) != null && !inputLine.matches("User-agent:(.*)")) {
+                        inputLineArr = inputLine.split(" ");
+                        if (inputLineArr[0].equals("Disallow:")) {
+                            if (inputLineArr[1] == null) {
+                                // accept all (do nothing)
+                            } else if (inputLineArr[1].equals("/")) {
+                                // Refuse all
+                                //TODO:what to do
+                            } else {
+                                Disallow.add(new URL(base + inputLineArr[1].trim()));
+                            }
+                        } else if (inputLineArr[0].equals("Crawl-delay:")) {
+                            crawlDelay = Integer.parseInt(inputLineArr[1]);
+                        } else if (inputLineArr[0].equals("Allow:")) {
+                            Allow.add(new URL(base + inputLineArr[1].trim()));
+                        }
+                    }
+                }
+            }
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(RobotTxtHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(RobotTxtHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        /* Closing the BufferReader */
+        try {
+            robotData.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-		/* Closing the BufferReader */
-		try {
-			robotData.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public ArrayList<URL> getDisallowed() {
+        return Disallow;
+    }
 
-	public ArrayList<URL> getDisallowed() {
-		return Disallow;
-	}
-	public ArrayList<URL> getAllowed() {
-		return Allow;
-	}
-	public Integer getCrawlDelay(){
-		return crawlDelay;
-	}
+    public ArrayList<URL> getAllowed() {
+        return Allow;
+    }
+
+    public Integer getCrawlDelay() {
+        return crawlDelay;
+    }
 }
