8c6007f5cd058a45d5af7d1880d3e28bf29fa9fd
AntounWagdy <AntounWagdy@gmail.com>
2017-03-26 00:08:21 +0200

    Phase 1 : deleting some unnecessary files

--- src/WebSearchEngine/WebSearchEngine.java
+++
@@ -1,9 +0,0 @@
-package WebSearchEngine;
-
-public class WebSearchEngine {
-
-    public static void main(String[] args) {
-        Program crawler_and_indexer = new Program();
-        crawler_and_indexer.run_search_Engine();
-    }
-}
